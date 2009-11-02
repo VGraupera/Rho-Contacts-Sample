b963879e47dffcf1997c1770ed7803a0e0917926
Vidal Graupera <vidal@vdggroup.com>
2009-11-01 19:02:41 -0800

    first commit

---
+++ app/Contact/config.rb
@@ -0,0 +1,3 @@
+require 'rho'
+
+Rho::RhoConfig::add_source("Contact", {"url"=>"", "source_id"=>8078})
---
+++ app/Contact/controller.rb
@@ -0,0 +1,48 @@
+require 'rho/rhocontroller'
+require 'rho/rhocontact'
+
+class ContactController < Rho::RhoController
+
+  #GET /Contact
+  def index
+    @contacts = Rho::RhoContact.find(:all)
+    @contacts.to_a.sort! {|x,y| x[1]['first_name'] <=> y[1]['first_name'] } if @contacts
+    
+    render
+  end
+
+  # GET /Contact/{1}
+  def show
+    @contact = Rho::RhoContact.find(@params['id'])
+    render :action => :show
+  end
+
+  # GET /Contact/new
+  def new
+    render :action => :new
+  end
+
+  # GET /Contact/{1}/edit
+  def edit
+    @contact = Rho::RhoContact.find(@params['id'])
+    render :action => :edit
+  end
+
+  # POST /Contact/create
+  def create
+    @contact = Rho::RhoContact.create!(@params['contact'])
+    redirect :action => :index
+  end
+
+  # POST /Contact/{1}/update
+  def update
+    Rho::RhoContact.update_attributes(@params['contact'])
+    redirect :action => :index
+  end
+
+  # POST /Contact/{1}/delete
+  def delete
+    Rho::RhoContact.destroy(@params['id'])
+    redirect :action => :index
+  end
+end
